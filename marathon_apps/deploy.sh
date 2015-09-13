4655cca85f79581296763cfe5d6baaf8f93dff14
malnick <malnick@gmail..com>
2015-09-12 20:21:27 -0700

    deploy script

---
+++ marathon_apps/deploy.sh
@@ -0,0 +1,17 @@
+#/bin/bash
+
+case "$1" in
+  microbrew)
+    curl -XPOST -H 'Content-Type: application/json' -d @microbrew.json streaker.technoblogic.io:8080/v2/apps/
+    ;;
+  micropig)
+    curl -XPOST -H 'Content-Type: application/json' -d @micropig.json streaker.technoblogic.io:8080/v2/apps/
+    ;;
+  microscope)
+    curl -XPOST -H 'Content-Type: application/json' -d @microscope.json streaker.technoblogic.io:8080/v2/apps/
+    ;;
+  streaker)
+    curl -XPOST -H 'Content-Type: application/json' -d @micro-streaker.json streaker.technoblogic.io:8080/v2/apps/
+    ;;
+esac
+exit 0
